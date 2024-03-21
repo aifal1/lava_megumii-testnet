664362259ab221a19f920a5fd32981a2c2c3d72b
aifal1 <80118910+aifal1@users.noreply.github.com>
2024-03-21 23:02:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-4a68ffb8-39d3-4c0f-87d7-e9a04ee209d4/)
 
 
 echo "$curl_output" >> Update.tsx
